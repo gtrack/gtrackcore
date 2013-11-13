e3901207363ab9a2817a3f25f7c7b76b2ea04cc2
Sveinung Gundersen <sveinugu@gmail.com>
2013-11-13 15:49:11 +0100

    Fixed bug in PreProcessTracksJob

--- gtrackcore/preprocess/PreProcessTracksJob.py
+++ gtrackcore/preprocess/PreProcessTracksJob.py
@@ -24,9 +24,9 @@ from gtrackcore.util.CustomExceptions import NotSupportedError, AbstractClassErr
 
 class PreProcessTracksJob(object):
     VERSION = '1.0'
-    
+
     PASS_ON_EXCEPTIONS = False
-    
+
     def __init__(self, genome, username='', mode='Real', raiseIfAnyWarnings=False):
         self._genome = genome
         self._username = username
@@ -34,57 +34,57 @@ class PreProcessTracksJob(object):
         self._status = ''
         self._raiseIfAnyWarnings = raiseIfAnyWarnings
         self._warningTrackNames = []
-    
+
     def process(self):
         assert self._genome is not None, 'Error: genome must be specified when preprocessing tracks.'
-                        
+
         atLeastOneFinalized = False
         for trackName in self._allTrackNames():
             assert trackName != ['']
             overlapRulesProcessedForTrackName = []
             collector = PreProcMetaDataCollector(self._genome, trackName)
-            
+
             try:
                 trackName = self._renameTrackNameIfIllegal(trackName)
-                
+
                 for allowOverlaps in [True, False]:
                     anyGeSourceManagers = False
                     for geSourceManager in self._allGESourceManagers(trackName, allowOverlaps):
                         anyGeSourceManagers = True
-                        
+
                         # PreProcess if needed
                         if self._shouldPreProcess():
                             PreProcessUtils.removeOutdatedPreProcessedFiles(self._genome, trackName, allowOverlaps, self._mode)
-                            
+
                             if self._shouldPrintProcessMessages() and allowOverlaps not in overlapRulesProcessedForTrackName:
                                 self._printProcessTrackMessage(trackName, allowOverlaps)
                                 overlapRulesProcessedForTrackName.append(allowOverlaps)
-                            
+
                             self._status = 'Trying to preprocess geSource...'
                             geSourceJob = PreProcessGeSourceJob(trackName, geSourceManager, allowOverlaps, self._mode)
                             anyWarnings = geSourceJob.process()
-                            
+
                             if self._raiseIfAnyWarnings and anyWarnings and trackName not in self._warningTrackNames:
                                 self._warningTrackNames.append(trackName)
-                                
+
                             collector.updatePreProcDirtyStatus(geSourceJob.hasModifiedData())
-                    
+
                     # Finalize overlapRule output if needed
                     if anyGeSourceManagers and self._shouldFinalize() and collector.preProcIsDirty():
                         if self._mode == 'Real' and self._shouldMergeChrFolders():
                             self._status = 'Trying to combine chromosome vectors into combined vectors.'
                             PreProcessUtils.createBoundingRegionShelve(self._genome, trackName, allowOverlaps)
                             ChrMemmapFolderMerger.merge(self._genome, trackName, allowOverlaps)
-                            
+
                             self._status = 'Trying to remove chromosome folders'
                             PreProcessUtils.removeChrMemmapFolders(self._genome, trackName, allowOverlaps)
-                                
+
                         self._status = 'Trying to check whether 3D data is correct'
                         PreProcessUtils.checkIfEdgeIdsExist(self._genome, trackName, allowOverlaps)
                         PreProcessUtils.checkUndirectedEdges(self._genome, trackName, allowOverlaps)
                         PreProcessUtils.checkUndirectedEdges(self._genome, trackName, allowOverlaps)
                         collector.markOverlapRuleAsFinalized(allowOverlaps)
-                            
+
                 # Finalize track if needed
                 if self._shouldFinalize():
                     if collector.preProcIsDirty():
@@ -93,7 +93,7 @@ class PreProcessTracksJob(object):
                         if not atLeastOneFinalized:
                             atLeastOneFinalized = True
                     else:
-                        collector.removeEntry()                    
+                        collector.removeEntry()
 
             except NotSupportedError, e:
                 collector.removeEntry()
@@ -106,10 +106,10 @@ class PreProcessTracksJob(object):
                 if self.PASS_ON_EXCEPTIONS:
                     raise
                 else:
-                    self._printExceptionMsg(e, trackName, Error=True)                
-                
+                    self._printExceptionMsg(e, trackName, Error=True)
+
             self._calcAndStoreSubTrackCount(trackName)
-            
+
         if self._raiseIfAnyWarnings and len(self._warningTrackNames) > 0:
             raise Warning('Warnings occurred in the following tracks: ' + \
                           ', '.join(prettyPrintTrackName(tn) for tn in self._warningTrackNames))
@@ -117,7 +117,7 @@ class PreProcessTracksJob(object):
 
     def _allTrackNames(self):
         raise AbstractClassError
-        
+
     def _allGESourceManagers(self, trackName, allowOverlaps):
         collector = PreProcMetaDataCollector(self._genome, trackName)
         if allowOverlaps == False and collector.overlapRuleHasBeenFinalized(True):
@@ -131,57 +131,57 @@ class PreProcessTracksJob(object):
                     tf = TrackFormat.createInstanceFromGeSource(geSource)
                     if tf.isDense() or geSource.hasNoOverlappingElements():
                         return
-                
+
                 self._status = 'Trying to prepare preprocessing for track "%s"' % ':'.join(trackName) + \
                                 (' (filename: "%s")' % geSource.getFileName() if geSource.hasOrigFile() else '') + \
                                 (' (allowOverlaps: %s)' % allowOverlaps)
                 if PreProcessUtils.shouldPreProcessGESource(trackName, geSource, allowOverlaps):
                     yield self._getGESourceManagerFromGESource(geSource)
-    
+
     def _allGESources(self, trackName):
         raise AbstractClassError
-        
+
     def _getGESourceManagerFromGESource(self, geSource):
         return GESourceManager(geSource)
-        
+
     def _getGESourceManagerFromTrack(self, trackName):
         origBrTuples = PreProcMetaDataCollector(self._genome, trackName).\
                         getBoundingRegionTuples(allowOverlaps=True)
         return OverlapClusteringGESourceManager(self._genome, trackName, origBrTuples)
-        
+
     def _shouldPreProcess(self):
         return True
-        
+
     def _shouldPrintProcessMessages(self):
         return True
-        
+
     def _shouldFinalize(self):
         return True
-        
+
     def _shouldMergeChrFolders(self):
         return True
-        
+
     def _renameTrackNameIfIllegal(self, trackName):
         legalTrackName = [replaceIllegalElementsInTrackNames(x) for x in trackName]
-        
+
         if legalTrackName != trackName and os.path.exists(createDirPath(trackName, self._genome)):
             renameTrack(self._genome, trackName, legalTrackName)
-            
+
         return legalTrackName
-        
+
     def _updateAllChrs(self, allChrs, chrList, allowOverlaps):
         if not allowOverlaps in allChrs:
             allChrs[allowOverlaps] = set()
         assert all(chr not in allChrs[allowOverlaps] for chr in chrList), \
             'Error: chromosome %s already preprocessed for previous GenomeElementSource (this may be because of elements of the same chromosome is found in different files).' % chr
         allChrs[allowOverlaps].update(set(chrList))
-    
+
     def _calcAndStoreSubTrackCount(self, trackName):
         ti = TrackInfo(self._genome, trackName)
         if ti.isValid():
             ti.subTrackCount = 1
             ti.store()
-    
+
     def _printProcessTrackMessage(self, trackName, allowOverlaps):
         if self._mode == 'Simulated':
             print "Would now have processed track: '%s' with allowOverlaps: %s in a real run." % (':'.join(trackName), allowOverlaps)
@@ -189,7 +189,7 @@ class PreProcessTracksJob(object):
             print "Only updating meta info based on track: '%s' with allowOverlaps: %s" % (':'.join(trackName), allowOverlaps)
         elif self._mode == 'Real':
             print "Processing track: '%s' with allowOverlaps: %s" % (':'.join(trackName), allowOverlaps)
-    
+
     def _printExceptionMsg(self, e, trackName, Error=False):
         print (os.linesep + '--- BEGIN ERROR ---' + os.linesep *2 if Error else 'Warning! ') + \
             "Could not pre-process track '%s'." % ':'.join(trackName)
@@ -202,14 +202,16 @@ class PreProcessTracksJob(object):
 class PreProcessAllTracksJob(PreProcessTracksJob):
     def __init__(self, genome, trackNameFilter=[], username='', mergeChrFolders=True, **kwArgs):
         PreProcessTracksJob.__init__(self, genome, username=username, **kwArgs)
+        if trackNameFilter == ['']:
+            trackNameFilter = []
         self._trackNameFilter = trackNameFilter
         self._mergeChrFolders = mergeChrFolders
-        
+
     def _allTrackNames(self):
         #avoidLiterature = len(self._trackNameFilter) == 0 or (self._trackNameFilter != GenomeInfo.getLiteratureTrackName(self._genome))
         trackSource = OrigTrackNameSource(self._genome, self._trackNameFilter, avoidLiterature=False)
         return reorderTrackNameListFromTopDownToBottomUp(trackSource)
-        
+
     def _allGESources(self, trackName):
         baseDir = createOrigPath(self._genome, trackName)
 
@@ -220,14 +222,14 @@ class PreProcessAllTracksJob(PreProcessTracksJob):
             self._status = 'Checking file: ' + fn
             if os.path.isdir(fn):
                 continue
-            
+
             fnPart = os.path.split(fn)[-1]
             if fnPart[0] in ['.','_','#'] or fnPart[-1] in ['~','#']: #to avoid hidden files..
                 continue
 
             self._status = 'Trying to create geSource from fn: ' + fn
             yield GenomeElementSource(fn, self._genome, forPreProcessor=True)
-    
+
     def _calcAndStoreSubTrackCount(self, trackName):
         ti = TrackInfo(self._genome, trackName)
         trackCount = 0
@@ -239,11 +241,11 @@ class PreProcessAllTracksJob(PreProcessTracksJob):
             trackCount += 1
         ti.subTrackCount = trackCount
         ti.store()
-    
+
     def _shouldMergeChrFolders(self):
         return self._mergeChrFolders
-        
-    
+
+
 class PreProcessExternalTrackJob(PreProcessTracksJob):
     PASS_ON_EXCEPTIONS = True
 
@@ -252,10 +254,10 @@ class PreProcessExternalTrackJob(PreProcessTracksJob):
         self._fullFn = fullFn
         self._extTrackName = extTrackName
         self._fileSuffix = fileSuffix
-        
+
     def _allTrackNames(self):
         return [self._extTrackName]
-        
+
     def _allGESources(self, trackName):
         return [ExternalTrackManager.getGESource(self._fullFn, self._fileSuffix, \
                                                  self._extTrackName, self._genome, printWarnings=True)]
@@ -274,15 +276,15 @@ class PreProcessCustomTrackJob(PreProcessTracksJob):
         self._preProcess = preProcess
         self._finalize = finalize
         self._mergeChrFolders = mergeChrFolders
-        
+
     def _allTrackNames(self):
         return [self._trackName]
-        
+
     def _allGESources(self, trackName):
         regionList = self._regionList if self._preProcess else [self._regionList[0]]
         for region in regionList:
             yield self._getGeSourceCallBackFunc(self._genome, self._trackName, region, **self._callBackArgs)
-    
+
     def _getGESourceManagerFromGESource(self, geSource):
         tf = TrackFormat.createInstanceFromGeSource(geSource)
         if tf.reprIsDense():
@@ -294,28 +296,28 @@ class PreProcessCustomTrackJob(PreProcessTracksJob):
         else:
             return RegionBasedGESourceManager(geSource, self._regionList, \
                                               calcStatsInExtraPass=True, countElsInBoundingRegions=True)
-        
+
     def _getGESourceManagerFromTrack(self, trackName):
         raise NotSupportedError
-        
+
     def _shouldPreProcess(self):
         return self._preProcess
-        
+
     def _shouldPrintProcessMessages(self):
         return False
 
     def _shouldFinalize(self):
         return self._finalize
-    
+
     def _shouldMergeChrFolders(self):
         return self._mergeChrFolders
-    
+
 
 if __name__ == "__main__":
     if not len(sys.argv) in [2,3,4]:
         print 'Syntax: python PreProcessTracksJob.py genome [trackName:subType] [mode=Real/Simulated/UpdateMeta]'
         sys.exit(0)
-        
+
     if len(sys.argv) == 2:
         tn = []
         mode = 'Real'
