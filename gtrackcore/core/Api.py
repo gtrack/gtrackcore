d6cf7da12e443c93e3a067221c786f54a5391365
brynjagr <brynjagr@iterate.no>
2014-01-14 19:56:07 +0100

    an attempt at creating a function for counting # of track elements for a given track

--- gtrackcore/core/Api.py
+++ gtrackcore/core/Api.py
@@ -1,10 +1,10 @@
-import shutil, os, sys
+import shutil, os, sys, numpy
 
 def importFile(fileName, genome, trackName):
     """fileName genome trackName"""
 
     trackName = _convertTrackName(trackName)
-    
+
     from gtrackcore.util.CommonFunctions import createOrigPath, ensurePathExists
     origFn = createOrigPath(genome, trackName, os.path.basename(fileName))
     if os.path.exists(origFn):
@@ -12,14 +12,14 @@ def importFile(fileName, genome, trackName):
     ensurePathExists(origFn)
     shutil.copy(fileName, origFn)
     os.chmod(origFn, 0664)
-    
+
     from gtrackcore.preprocess.PreProcessTracksJob import PreProcessAllTracksJob
     PreProcessAllTracksJob(genome, trackName).process()
 
 def _convertTrackName(trackName):
     from gtrackcore.util.CommonFunctions import convertTNstrToTNListFormat
     return convertTNstrToTNListFormat(trackName, doUnquoting=True)
-    
+
 def _trackNameExists(genome, trackName):
     from gtrackcore.track.hierarchy.ProcTrackOptions import ProcTrackOptions
     if not ProcTrackOptions.isValidTrack(genome, trackName):
@@ -40,29 +40,29 @@ def listAvailableGenomes():
     for dir in os.listdir(dirPath):
         if dir[0] != '.':
             print '    ' + dir
-    
+
 def listAvailableTracks(genome):
     "genome"
     print 'List of available tracks for genome "%s":' % genome
     _getDirPath(genome)
-    
+
     from gtrackcore.track.hierarchy.ProcTrackNameSource import ProcTrackNameSource
     for trackName in ProcTrackNameSource(genome):
         print '    ' + ':'.join(trackName)
-    
+
 def getExtractionOptions(genome, trackName):
     """genome trackName"""
-    
+
     trackName = _convertTrackName(trackName)
     if not _trackNameExists(genome, trackName):
         return
-    
+
     print
     print 'Available extraction options for track "%s" of genome "%s":' % (':'.join(trackName), genome)
     print
     print '{:<19}'.format('fileFormatName') + '{:<17}'.format('allowOverlaps') + 'Description'
     print '-'*80
-    
+
     from gtrackcore.extract.TrackExtractor import TrackExtractor
     for text, suffix in TrackExtractor.getTrackExtractionOptions(genome, trackName):
         fileFormatName, asOriginal, allowOverlaps = TrackExtractor.getAttrsFromExtractionFormat(text)
@@ -72,7 +72,7 @@ def _commonExportFile(outFileName, genome, trackName, fileFormatName, allowOverl
     trackName = _convertTrackName(trackName)
     if not _trackNameExists(genome, trackName):
         return
-        
+
     outFileName = os.path.abspath(outFileName)
 
     from gtrackcore.extract.TrackExtractor import TrackExtractor
@@ -82,21 +82,37 @@ def _commonExportFile(outFileName, genome, trackName, fileFormatName, allowOverl
 
 def exportFile(outFileName, genome, trackName, fileFormatName, allowOverlaps):
     """outFileName genome trackName fileFormatName allowOverlaps"""
-    
+
     from gtrackcore.input.userbins.UserBinSource import UserBinSource
     bins = UserBinSource('*', '*', genome, includeExtraChrs=True)
-    
+
     _commonExportFile(outFileName, genome, trackName, fileFormatName, allowOverlaps, bins)
 
 def exportFileInRegion(outFileName, genome, trackName, fileFormatName, allowOverlaps, region):
     """outFileName genome trackName fileFormatName allowOverlaps region (e.g. chr21:1m-2m)"""
-    
+
     from gtrackcore.input.userbins.UserBinSource import UserBinSource
     bins = UserBinSource(region, '*', genome, includeExtraChrs=True)
-    
+
     _commonExportFile(outFileName, genome, trackName, fileFormatName, allowOverlaps, bins)
 
-    
+
+def _getTrackData(genome, trackName, chr, allowOverlaps):
+    from gtrackcore.track.memmap.TrackSource import TrackSource
+    trackSource = TrackSource()
+    return trackSource.getTrackData(trackName, genome, chr, allowOverlaps.lower() in ('yes', 'true', 't', '1'))
+
+
+def countTrackElements(genome, trackName, chr, allowOverlaps):
+    trackName = _convertTrackName(trackName)
+    if not _trackNameExists(genome, trackName):
+        print 'There is no track %s for genome %s' % (trackName, genome)
+        return
+    trackData = _getTrackData(genome, trackName, chr, allowOverlaps)
+
+    print numpy.shape(trackData['start'])[0]
+
+
 def _usage():
     print 'syntax: '
     print 'to use: [name] [args]'
