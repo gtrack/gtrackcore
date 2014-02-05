d4e72cea130487a97eabc15a35bac0ab08a6d6af
henrigs <henrigs@ifi.uio.no>
2014-02-05 16:20:59 +0100

    Stores bounding region info

--- gtrackcore/track/pytables/BoundingRegionContainer.py
+++ gtrackcore/track/pytables/BoundingRegionContainer.py
@@ -47,94 +47,50 @@ class BoundingRegionContainer(object):
     def store_bounding_regions(self, bounding_region_tuples, genome_element_chr_list, sparse):
         assert sparse in [False, True]
 
-        temp_contents = OrderedDict()
+        temp_bounding_regions = self._create_bounding_regions_triples(bounding_region_tuples, genome_element_chr_list, sparse)
 
-        genome_element_chrs = set(genome_element_chr_list)
-        last_region = None
-        chr_start_idxs = OrderedDict()
-        chr_end_idxs = OrderedDict()
-        tot_el_count = 0
-        tot_bin_count = 0
-
-        db_handler = BoundingRegionCreationDatabaseHandler()
         table_description = self._create_table_description()
+        db_handler = BoundingRegionCreationDatabaseHandler()
+        db_handler.open()
         db_handler.create_table(table_description, len(bounding_region_tuples))
-        row = self._db_handler.get_row()
 
+        row = db_handler.get_row()
+        for br in temp_bounding_regions:
+            row['seqid'] = br[0]
+            row['start'] = br[1]
+            row['end'] = br[2]
+            row.append()
+
+        db_handler.close()
+
+    @staticmethod
+    def _create_bounding_regions_triples(bounding_region_tuples, genome_element_chr_list, sparse):
+        last_region = None
+        temp_bounding_regions = []
         for br in bounding_region_tuples:
-            if last_region is None or br.region.chr != last_region.chr:
-                if br.region.chr in temp_contents:
-                    raise InvalidFormatError("Error: bounding region (%s) is not grouped with previous bounding regions of the same chromosome (sequence)." % br.region)
-
-                last_region = None
-                temp_contents[br.region.chr] = OrderedDict()
-                if sparse:
-                    chr_start_idxs[br.region.chr] = tot_el_count
-            else:
-                if br.region < last_region:
-                    raise InvalidFormatError("Error: bounding regions in the same chromosome (sequence) are unsorted: %s > %s." % (last_region, br.region))
-                if last_region.overlaps(br.region):
-                    raise InvalidFormatError("Error: bounding regions '%s' and '%s' overlap." % (last_region, br.region))
-                if last_region.end == br.region.start:
-                    raise InvalidFormatError("Error: bounding regions '%s' and '%s' are adjoining (there is no gap between them)." % (last_region, br.region))
-
+            if br.region < last_region:
+                raise InvalidFormatError("Error: bounding regions in the same chromosome (sequence) are unsorted: %s > %s." % (last_region, br.region))
+            if last_region.overlaps(br.region):
+                raise InvalidFormatError("Error: bounding regions '%s' and '%s' overlap." % (last_region, br.region))
+            if last_region.end == br.region.start:
+                raise InvalidFormatError("Error: bounding regions '%s' and '%s' are adjoining (there is no gap between them)." % (last_region, br.region))
             if len(br.region) < 1:
                 raise InvalidFormatError("Error: bounding region '%s' does not have positive length." % br.region)
-
             if not sparse and len(br.region) != br.elCount:
                 raise InvalidFormatError("Error: track type representation is dense, but the length of bounding region '%s' is not equal to the element count: %s != %s" % (br.region, len(br.region), br.elCount))
 
-            startIdx, endIdx = (tot_el_count, tot_el_count + br.elCount) if not sparse else (None, None)
-            tot_el_count += br.elCount
-            if sparse:
-                chr_end_idxs[br.region.chr] = tot_el_count
-
-            temp_contents[br.region.chr][br.region.start] = BoundingRegionInfo(br.region.start, br.region.end, startIdx, endIdx, 0, 0)
+            temp_bounding_regions.append((br.region.chr, br.region.start, br.region.end))
 
             last_region = br.region
-
         if sparse:
-            tot_bin_count = 0
-            for chr in temp_contents:
-                chrLen = GenomeInfo.getChrLen(self._genome, chr)
-                numBinsInChr = CompBinManager.getNumOfBins(GenomeRegion(start=0, end=chrLen))
-                for key in temp_contents[chr].keys():
-                    startBinIdx = tot_bin_count
-                    endBinIdx = tot_bin_count + numBinsInChr
-                    brInfo = temp_contents[chr][key]
+            diff = set(genome_element_chr_list) - set([br_triple[0] for br_triple in temp_bounding_regions])
+            if len(diff) > 0:
+                raise InvalidFormatError('Error: some chromosomes (sequences) contains data, but has no bounding regions: %s' % ', '.join(diff))
 
-                    if chr in genome_element_chrs:
-                        temp_contents[chr][key] = BoundingRegionInfo(brInfo.start, brInfo.end, \
-                                                                    chr_start_idxs[chr], chr_end_idxs[chr], \
-                                                                    startBinIdx, endBinIdx)
-                    else:
-                        if chr_end_idxs[chr] - chr_start_idxs[chr] > 0:
-                            raise InvalidFormatError("Error: bounding region '%s' has incorrect element count: %s > 0" % (GenomeRegion(chr=chr, start=brInfo.start, end=brInfo.end), chr_end_idxs[chr] - chr_start_idxs[chr]))
-                        temp_contents[chr][key] = BoundingRegionInfo(brInfo.start, brInfo.end, 0, 0, 0, 0)
+        return temp_bounding_regions
 
-                if chr in genome_element_chrs:
-                    tot_bin_count += numBinsInChr
-
-        if len(genome_element_chrs - set(temp_contents.keys())) > 0:
-            raise InvalidFormatError('Error: some chromosomes (sequences) contains data, but has no bounding regions: %s' % ', '.join(genome_element_chrs - set(temp_contents.keys())))
-
-        createPath(self._fn)
-
-        for chr in temp_contents:
-            brInfoDict = temp_contents[chr]
-            temp_contents[chr] = BrInfoHolder(tuple(brInfoDict.keys()), tuple(brInfoDict.values()))
-
-        brShelve = safeshelve.open(self._fn)
-        brShelve.update(temp_contents)
-        brShelve.close()
-
-        while not self.file_exists():
-            from gtrackcore.application.LogSetup import logMessage
-            logMessage("Bounding region shelve file '%s' has yet to be created" % self._fn)
-            import time
-            time.sleep(0.2)
-
-    def _create_table_description(self):
+    @staticmethod
+    def _create_table_description():
         return {
                 'seqid': tables.StringCol(100),
                 'start': tables.Int32Col(),
