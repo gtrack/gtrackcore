fcf0fd7a0d6259a5b8ad66fb011c37457d50623d
henrigs <henrigs@ifi.uio.no>
2014-02-18 13:44:18 +0100

    added timeit_repeat decorator

--- gtrackcore/util/CustomDecorators.py
+++ gtrackcore/util/CustomDecorators.py
@@ -1,8 +1,13 @@
 import time
+import functools
 
 
-def timeit(func, print_args=False):
+def timeit(func=None, print_args=False):
 
+    if func is None:
+        return functools.partial(timeit, print_args=print_args)
+
+    @functools.wraps(func)
     def wrapper(*args, **kw_args):
         start = time.time()
         result = func(*args, **kw_args)
@@ -17,8 +22,12 @@ def timeit(func, print_args=False):
     return wrapper
 
 
-def print_args(func, pretty_print=False):
+def print_args(func=None, pretty_print=False):
 
+    if func is None:
+        return functools.partial(print_args, pretty_print=pretty_print)
+
+    @functools.wraps(func)
     def wrapper(*args, **kw_args):
         if pretty_print:
             print func.__name__
@@ -30,5 +39,28 @@ def print_args(func, pretty_print=False):
             print '%r (%r, %r)' % (func.__name__, args, kw_args)
 
         return func(*args, **kw_args)
+
+    return wrapper
+
+
+def timeit_repeat(func=None, reps=2):
+
+    if func is None:
+        return functools.partial(timeit_repeat, reps=reps)
+
+    @functools.wraps(func)
+    def wrapper(*args, **kw_args):
+        rep_times = []
+
+        for x in xrange(reps):
+            start = time.time()
+            result = func(*args, **kw_args)
+            end = time.time()
+            time_used = end - start
+            rep_times.append(time_used)
+
+        print '%r avg. time: %d' % (func.__name__, (sum(rep_times) / len(rep_times)))
+
+        return result
 
     return wrapper
