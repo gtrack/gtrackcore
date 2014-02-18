33dfa0beca2261efbe35ab892aa0cc43e21a6b1d
henrigs <henrigs@ifi.uio.no>
2014-02-18 13:46:34 +0100

    added timeit_repeat decorator

--- gtrackcore/util/CustomDecorators.py
+++ gtrackcore/util/CustomDecorators.py
@@ -41,3 +41,26 @@ def print_args(func=None, pretty_print=False):
         return func(*args, **kw_args)
 
     return wrapper
+
+
+def timeit_repeat(func=None, reps=2):
+
+    if func is None:
+        return functools.partial(timeit_repeat, reps=reps)
+
+    @functools.wraps(func)
+    def wrapper(*args, **kw_args):
+        rep_times = []
+
+        for x in xrange(reps):
+            start = time.time()
+            result = func(*args, **kw_args)
+            end = time.time()
+            time_used = end - start
+            rep_times.append(time_used)
+
+        print '%r avg. time: %d' % (func.__name__, (sum(rep_times) / len(rep_times)))
+
+        return result
+
+    return wrapper
