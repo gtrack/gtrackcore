3046ed27da814c25641d48e760461d0a300217b6
Brynjar Rongved <brynjar.rongved@gmail.com>
2014-03-06 00:07:26 +0100

    added function that count elements in all bounding regions to TrackTools.

--- gtrackcore/tools/TrackTools.py
+++ gtrackcore/tools/TrackTools.py
@@ -1,5 +1,6 @@
 from gtrackcore.track.core.Track import PlainTrack
 from gtrackcore.track.core.GenomeRegion import GenomeRegion
+from gtrackcore.track.pytables.BoundingRegionHandler import BoundingRegionHandler
 
 def get_track_view(track_name, genome_region):
     track = PlainTrack(track_name)
@@ -18,6 +19,14 @@ def intersection(track_view_1, track_view_2):
     pass
 
 
+def count_elements_in_all_bounding_regions(track_name, genome='TestGenome', allow_overlaps=False):
+    bounding_regions = BoundingRegionHandler(genome, track_name, allow_overlaps).get_all_bounding_regions()
+    track = PlainTrack(track_name)
+
+    num_elements = 0
+    for tv in [track.getTrackView(region) for region in bounding_regions]:
+        num_elements += count_elements(tv)
+    return num_elements
 
 def print_result(tool, track_name, result):
     print
@@ -27,14 +36,16 @@ def print_result(tool, track_name, result):
 
 
 if __name__ == '__main__':
-    track_name = ['testcat', 'kmerpoint']
+    track_name = ['testcat', 'kmer-point']
     genome_region_list = ['testgenome', 'chr21', 0, 46944323]
 
     genome_region = GenomeRegion(* genome_region_list)
     track_view = get_track_view(track_name, genome_region)
 
     num_elements = count_elements(track_view)
+    num_all_elements = count_elements_in_all_bounding_regions(track_name)
     bp_coverage = coverage(track_view)
 
     print_result("count_elements", track_name, num_elements)
+    print_result("count_all_elements", track_name, num_all_elements)
     print_result("coverage", track_name, bp_coverage)
