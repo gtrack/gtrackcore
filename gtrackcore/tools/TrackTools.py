537aa10e076b8afd50b844d8ade8ed4f986be283
Brynjar Rongved <brynjar.rongved@gmail.com>
2014-03-11 15:39:10 +0100

    added k_highest_values() to tools

--- gtrackcore/tools/TrackTools.py
+++ gtrackcore/tools/TrackTools.py
@@ -1,11 +1,7 @@
-import os
-import sys
-from gtrackcore.core.Config import Config
+from gtrackcore.tools.ToolExceptions import OperationNotSupportedError
 from gtrackcore.track.core.Track import PlainTrack
 from gtrackcore.track.core.GenomeRegion import GenomeRegion
 from gtrackcore.track.pytables.BoundingRegionHandler import BoundingRegionHandler
-from gtrackcore.util.CommonFunctions import createOrigPath, createPath, convertTNstrToTNListFormat
-from gtrackcore.util.CustomExceptions import ShouldNotOccurError
 
 
 def get_track_view(track_name, genome_region):
@@ -17,6 +13,20 @@ def count_elements(track_view):
     return track_view.getNumElements()
 
 
+def k_highest_values(track_view, k):
+    format_name = track_view.trackFormat.getFormatName()
+    if format_name not in ['Function', 'Linked function', 'Valued points', 'Linked valued segments', 'Valued segments',
+                       'Linked valued segments']:
+        raise OperationNotSupportedError
+    print len(track_view)
+    if k > track_view.getNumElements():
+        raise ValueError('The given k is larger than the number of elements of the TrackView')
+
+    values = track_view.valsAsNumpyArray()
+
+    return values.argsort()[-k:]
+
+
 def coverage(track_view):
     format_name = track_view.trackFormat.getFormatName()
     if format_name in ['Segments', 'Valued segments', 'Linked segments', 'Linked valued segments']:
@@ -26,7 +36,7 @@ def coverage(track_view):
     elif format_name in ['Genome partition', 'Step function', 'Linked genome partition', 'Linked step function', 'Linked base pairs']:
         return len(track_view)
     else:
-        raise ShouldNotOccurError
+        raise OperationNotSupportedError
 
 
 def intersection(track_view_1, track_view_2):
@@ -52,9 +62,11 @@ def print_result(tool, track_name, result):
 if __name__ == '__main__':
     genome = 'testgenome'
     track_name = ['testcat', 'point']
-    genome_region_list = [genome, 'chr21', 0, 36944323]
+    func_track =['testcat', 'test', 'function']
+    genome_region_list = [genome, 'chr21', 0, 46944323]
 
-    tv = get_track_view(track_name, GenomeRegion(*genome_region_list))
+    tv = get_track_view(func_track, GenomeRegion(*genome_region_list))
 
-    print coverage(tv)
+    print k_highest_values(tv, 3)
+
 
