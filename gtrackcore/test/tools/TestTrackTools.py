86adc22c9e125823a132ba76e2daf0cf56426c7b
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2014-03-19 19:23:43 +0100

    updated which tests we should write

--- gtrackcore/test/tools/TestTrackTools.py
+++ gtrackcore/test/tools/TestTrackTools.py
@@ -17,6 +17,26 @@ class TestTrackTools(unittest.TestCase):
     def test_coverage(self):
         pass
 
+    def test_count_elements(self):
+        pass
+
+    def test_count_elements_in_all_bounding_regions(self):
+        # Cannot be tested without real data in db
+        pass
+
+    def test_sum_of_values(self):
+        pass
+
+    def test_sum_of_weights(self):
+        # Cannot be tested without real data in db
+        pass
+
+    def test_sum_of_weights_with_iterator(self):
+        pass
+
+    def test_k_highest_values(self):
+        pass
+
 
 if __name__ == "__main__":
     unittest.main()
