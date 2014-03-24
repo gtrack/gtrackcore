2bfd8cdf951e6711b453dad923b01f7a9186db18
Brynjar Rongved <brynjar.rongved@gmail.com>
2014-03-24 17:53:45 +0100

    now use environment variable to find data directory

--- gtrackcore/core/Config.py
+++ gtrackcore/core/Config.py
@@ -10,52 +10,56 @@ class ConfigInit(type):
     def __getattribute__(cls, key):
         if not (type.__getattribute__(cls, '_INITIALIZED') or key == 'initialize'):
             type.__getattribute__(cls, 'initialize')()
-            
+
         return type.__getattribute__(cls, key)
 
 class Config(object):
     __metaclass__ = ConfigInit
 
     _INITIALIZED = False
-    
+
     @classmethod
-    def initialize(cls, configFileName='~/gtrackcore_config', dataDir='~/gtrackcore_data'):
+    def initialize(cls, configFileName=None, dataDir=None):
         cls._INITIALIZED = True
-        
+
+        gtrackcore_dir = os.environ.get('GTRACKCORE_DIR')
+
+        if not gtrackcore_dir:
+            if os.environ.get('HOME') is not None:
+                print 'GTRACKCORE_DIR env should be defined. Using %s as data directory' % os.environ.get('HOME')
+                gtrackcore_dir = os.environ.get('HOME')
+            else:
+                raise AttributeError('Neither env GTRACKCORE_DIR nor HOME is set')
+
+        data_dir = os.sep.join([gtrackcore_dir, 'gtrackcore_data'])
+        config_filename = os.sep.join([gtrackcore_dir, 'gtrackcore_config'])
+
         try:
-            if configFileName:
-                configFileName = os.path.expanduser(configFileName)
-            
-            if not dataDir:
-                raise ArgumentValueError('Data directory must be specified')
-            
-            dataDir = os.path.expanduser(dataDir)
-                
             configDef = OrderedDict()
-            
+
             configDef['General'] = OrderedDict( \
-                [('LOG_PATH', os.path.expanduser('~/gtrackcore_logs')), \
-                 ('ORIG_DATA_PATH', os.sep.join([dataDir, 'Original'])), \
-                 ('PROCESSED_DATA_PATH', os.sep.join([dataDir, 'Processed'])), \
-                 ('METADATA_FILES_PATH', os.sep.join([dataDir, 'Metadata'])), \
+                [('LOG_PATH', os.sep.join([gtrackcore_dir, 'gtrackcore_logs'])), \
+                 ('ORIG_DATA_PATH', os.sep.join([data_dir, 'Original'])), \
+                 ('PROCESSED_DATA_PATH', os.sep.join([data_dir, 'Processed'])), \
+                 ('METADATA_FILES_PATH', os.sep.join([data_dir, 'Metadata'])), \
                  ('MAX_CONCAT_LEN_FOR_OVERLAPPING_ELS', '20'), \
                  ('OUTPUT_PRECISION', '4'), \
                  ('USE_SLOW_DEFENSIVE_ASSERTS', 'False')])
-            
+
             configDef['Compatibility'] = OrderedDict( \
                 [('URL_PREFIX', '')])
-            
+
             configDef['Memmap'] = OrderedDict( \
                 [('COMP_BIN_SIZE', '100000'), \
                  ('MEMMAP_BIN_SIZE', str(1024 * 1024))])
-            
+
             cls._initConfig(configDef)
 
-            if configFileName:
-                if os.path.exists(configFileName):
-                    cls._readConfig(configFileName, configDef)            
-                cls._writeConfig(configFileName, configDef)
-        
+            if config_filename:
+                if os.path.exists(config_filename):
+                    cls._readConfig(config_filename, configDef)
+                cls._writeConfig(config_filename, configDef)
+
         except:
             cls._INITIALIZED = False
             raise
@@ -65,17 +69,17 @@ class Config(object):
         defaults = cls._getDefaultDict(configDef)
         config = SafeConfigParser(defaults)
         config.read(configFileName)
-        
+
         for section, section_vars in configDef.iteritems():
             if config.has_section(section):
                 for var, val in section_vars.iteritems():
                     configMethod = cls._getConfigParseMethod(val)
                     setattr(cls, var, getattr(config, configMethod)(section, var))
-    
+
     @classmethod
     def _initConfig(cls, configDef):
         defaults = cls._getDefaultDict(configDef)
-        
+
         for section, section_vars in configDef.iteritems():
             for var, val in section_vars.iteritems():
                 valType = cls._getValType(val)
@@ -88,22 +92,22 @@ class Config(object):
             config.add_section(section)
             for var in section_vars:
                 config.set(section, var, str(getattr(cls, var)))
-                
+
         with open(configFileName, 'wb') as configFile:
             config.write(configFile)
-            
+
     @classmethod
     def _getDefaultDict(cls, configDef):
         return OrderedDict(chain(*[configDef[section].iteritems() for section in configDef]))
-    
+
     @classmethod
     def _getConfigParseMethod(cls, val):
         return cls._commonGetParseMethod(val, returnConfigMethod=True)
-    
+
     @classmethod
     def _getValType(cls, val):
         return cls._commonGetParseMethod(val, returnConfigMethod=False)
-    
+
     @classmethod
     def _commonGetParseMethod(cls, val, returnConfigMethod):
         try:
@@ -118,5 +122,5 @@ class Config(object):
                     configMethod, valType = 'getboolean', bool
                 else:
                     configMethod, valType = 'get', str
-        
+
         return configMethod if returnConfigMethod else valType
