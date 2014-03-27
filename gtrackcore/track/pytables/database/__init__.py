eec235f1e9d33a94af5c51162e2d926842ad6939
Brynjar Rongved <brynjar.rongved@gmail.com>
2014-03-27 22:41:17 +0100

    Added an exit handler that closes remaining open pytables files (using atexit.register). This solves the problem with files not being closed at exit, which made pytables write a message to stderr. NB. how to close remaining file handlers has changed in version 3.1.0 of pytables, so pytables version >= 3.1.0 is now required

--- gtrackcore/track/pytables/database/__init__.py
+++ gtrackcore/track/pytables/database/__init__.py
@@ -1,1 +1,14 @@
-__author__ = 'henrigs'
+import tables
+import atexit
+
+@atexit.register
+def close_pytables_files():
+    open_files = tables.file._open_files
+
+    # make a copy of the open_files.handlers container for the iteration
+    handlers = list(open_files.handlers)
+    any_open_files = len(open_files.handlers) > 0
+
+    if any_open_files:
+        for fileh in handlers:
+            fileh.close()
