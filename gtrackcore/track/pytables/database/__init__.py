a95bcfb306b0cfc7eaae3eb353283147bdfef448
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2014-03-28 20:27:53 +0100

    The method for closing files in pytables is different before and after version 3.1.0. Added check and code for closing all files in versions of pytables < 3.1.0.

--- gtrackcore/track/pytables/database/__init__.py
+++ gtrackcore/track/pytables/database/__init__.py
@@ -3,12 +3,17 @@ import atexit
 
 @atexit.register
 def close_pytables_files():
-    open_files = tables.file._open_files
+    current_version = tuple(map(int, tables.__version__.split('.')))
+    if current_version >= (3, 1, 0):
+        open_files = tables.file._open_files
 
-    # make a copy of the open_files.handlers container for the iteration
-    handlers = list(open_files.handlers)
-    any_open_files = len(open_files.handlers) > 0
+        # make a copy of the open_files.handlers container for the iteration
+        handlers = list(open_files.handlers)
+        any_open_files = len(open_files.handlers) > 0
 
-    if any_open_files:
-        for fileh in handlers:
-            fileh.close()
+        if any_open_files:
+            for fileh in handlers:
+                fileh.close()
+    else:
+        for file in tables.file._open_files.values():
+            file.close()
