833888a1b1007fe35b1b1ac005781b0f2d94e7c4
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2014-04-02 17:23:50 +0200

    Added function for getting default value

--- gtrackcore/util/pytables/CommonNumpyFunctions.py
+++ gtrackcore/util/pytables/CommonNumpyFunctions.py
@@ -1,7 +1,22 @@
 import numpy
+from gtrackcore.util.CommonConstants import BINARY_MISSING_VAL
 
 
 def insert_into_array_of_larger_shape(array, shape):
     new_array = numpy.zeros(shape=shape, dtype=array.dtype)  # TODO: + gtrackcore default value for dtype
     new_array[[slice(0, shape_dimension) for shape_dimension in array.shape]] = array
     return new_array
+
+
+def get_default_numpy_value(dtype):
+    dtype = numpy.dtype(dtype)  # ensure numpy.dtype object
+    if dtype.kind == 'i':
+        return BINARY_MISSING_VAL
+    elif dtype.kind == 'f':
+        return numpy.nan
+    elif dtype.kind == 'S':
+        return ''
+    elif dtype.kind == 'b':
+        return False
+
+    raise ValueError('%s is not a valid data type' % str(dtype))
