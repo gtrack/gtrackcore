bd261e1cd14e4063e681e5ef1308cfcdf21e1e38
Brynjar Rongved <brynjar.rongved@gmail.com>
2014-04-09 11:24:17 +0200

    Bug fix in atexit function

--- gtrackcore/track/pytables/database/__init__.py
+++ gtrackcore/track/pytables/database/__init__.py
@@ -9,18 +9,23 @@ from gtrackcore.util.pytables.NameFunctions import get_genome_and_trackname
 @atexit.register
 def close_pytables_files():
     filenames = []
-    open_files = tables.file._open_files
 
     current_version = tuple(map(int, tables.__version__.split('.')))
     if current_version >= (3, 1, 0):
+
+        open_files = tables.file._open_files
+
+        # make a copy of the open_files.handlers container for the iteration
+        handlers = list(open_files.handlers)
         any_open_files = len(open_files.handlers) > 0
+
         if any_open_files:
-            filenames = open_files.filenames
-            for fileh in open_files.handlers:
+            filenames = tables.file._open_files.filenames
+            for fileh in handlers:
                 fileh.close()
     else:
         filenames = []
-        for filename, fileh in open_files.items():
+        for filename, fileh in tables.file._open_files.items():
             fileh.close()
             filenames.append(filename)
         for filename in filenames:
@@ -29,9 +34,13 @@ def close_pytables_files():
 
     _persist_metadata(filenames)
 
+
 def _persist_metadata(filenames):
     for filename in filenames:
-        genome, track_name = get_genome_and_trackname(filename)
+        try:
+            genome, track_name = get_genome_and_trackname(filename)
+        except IndexError:
+            continue
         dynamic_trackinfo = DynamicTrackInfo(genome, track_name)
         metadata_handler = MetadataHandler(genome, track_name)
         try:
