5de543da4a69083354dccc094d60c1c4aaf32ccd
Brynjar Rongved <brynjar.rongved@gmail.com>
2014-04-11 14:45:35 +0200

    Now extract archives with multiple members to a directory + refactoring

--- gtrackcore/tools/suite/GTrackCoreSuite.py
+++ gtrackcore/tools/suite/GTrackCoreSuite.py
@@ -1,12 +1,12 @@
 import os
-import shutil
 import tarfile
 import zipfile
-import errno
 
 from gtrackcore.core.Config import Config
 from gtrackcore.tools.suite.FileParser import parse_track_resource_file
 from gtrackcore.tools.suite.ResourceRetrieval import retrieve_resources
+from gtrackcore.util.suite.CommonFunctions import custom_splitext, get_filename_and_file_ext, remove_file_or_dir, \
+    copy_file_or_dir
 
 
 def load_track_suite(suite_filename):
@@ -29,34 +29,12 @@ def load_track_suite(suite_filename):
         print 'Copying %d resources.' % len(not_compressed_fetched_resources)
         for resource in not_compressed_fetched_resources:
             to_file_path = temp_resource_dir + os.sep + resource['file_path'].split(os.sep)[-1]
-            _copy_file_or_dir(resource['file_path'], to_file_path)
+            copy_file_or_dir(resource['file_path'], to_file_path)
 
     if len(compressed_fetched_resources) > 0:
         print 'Extracting %d archives.' % len(compressed_fetched_resources)
         for resource in compressed_fetched_resources:
             _decompress_archive(resource['file_path'], temp_resource_dir)
-
-
-def _copy_file_or_dir(src, dest):
-    try:
-        shutil.copytree(src, dest)
-    except OSError as e:
-        # If the error was caused because the source wasn't a directory
-        if e.errno == errno.ENOTDIR:
-            shutil.copy(src, dest)
-        else:
-            print('Unable to copy %s to %s. Error: %s' % (e, src, dest))
-
-
-def _remove_file_or_dir(path):
-    try:
-        shutil.rmtree(path)
-    except OSError as e:
-        # If the error was caused because the source wasn't a directory
-        if e.errno == errno.ENOTDIR:
-            os.remove(path)
-        else:
-            print('Unable to delete %s. Error: %s' % (path, e))
 
 
 def _setup_dirs():
@@ -66,34 +44,59 @@ def _setup_dirs():
 
     temp_resource_dir = Config.RESOURCE_DIR
     if os.path.exists(temp_resource_dir):
-        _remove_file_or_dir(temp_resource_dir)
+        remove_file_or_dir(temp_resource_dir)
     os.makedirs(temp_resource_dir)
 
     return download_dir, temp_resource_dir
 
 
 def _decompress_archive(file_path, dest_dir):
-    _, file_ext = os.path.splitext(file_path)
 
-    file_name = file_path.split('/')[-1]
+    file_name, file_ext =get_filename_and_file_ext(file_path)
     print 'Trying to extract "%s"...' % file_name,
-
     if tarfile.is_tarfile(file_path):
-        if file_ext == '.gz':
-            tar = tarfile.open(file_path, mode='r:gz')
-        else:
-            tar = tarfile.open(file_path, mode='r')
-        tar.extractall(dest_dir)
-        tar.close()
+        _decompress_tarfile(file_path, dest_dir)
     elif zipfile.is_zipfile(file_path):
-        zip = zipfile.ZipFile(file_path, 'r')
-        zip.extractall(dest_dir)
-        zip.close()
+        _decompress_zipfile(file_path, dest_dir)
     else:
         raise TypeError('Archive type not supported: %s' % file_ext)
 
     print 'Done!'
 
 
+def _decompress_tarfile(file_path, dest_dir):
+
+    file_name, file_ext =get_filename_and_file_ext(file_path)
+
+    if file_ext == '.gz':
+        tar = tarfile.open(file_path, mode='r:gz')
+    else:
+        tar = tarfile.open(file_path, mode='r')
+    members = tar.getmembers()
+    if len(members) > 1:
+        target_dir = dest_dir + '/' + custom_splitext(file_name)[0]
+        os.makedirs(target_dir)
+        for member in members:
+            tar.extract(member, target_dir)
+    else:
+        tar.extractall(dest_dir)
+        tar.close()
+
+
+def _decompress_zipfile(file_path, dest_dir):
+    file_name, file_ext =get_filename_and_file_ext(file_path)
+
+    zip = zipfile.ZipFile(file_path, 'r')
+    members = zip.namelist()
+    if len(members) > 1:
+        target_dir = dest_dir + '/' + custom_splitext(file_name)[0]
+        os.makedirs(target_dir)
+        for member in members:
+            zip.extract(member, target_dir)
+    else:
+        zip.extractall(dest_dir)
+        zip.close()
+
+
 if __name__ == '__main__':
-    load_track_suite('/ifi/asgard/m00/henrigs/lol.suite')
+    load_track_suite('/Users/brynjar/lol.gsuite')
--- gtrackcore/tools/suite/ResourceRetrieval.py
+++ gtrackcore/tools/suite/ResourceRetrieval.py
@@ -1,8 +1,9 @@
 from copy import copy
 import os
-import re
 import urllib2
 from urlparse import urlparse
+
+from gtrackcore.util.suite.CommonFunctions import get_new_enumerated_filename, path_is_dir
 
 
 def retrieve_resources(track_resources, download_path):
@@ -13,7 +14,7 @@ def retrieve_resources(track_resources, download_path):
         scheme = parsed_url.scheme
 
         if scheme in ['http', 'file', 'ftp']:
-            if not _path_is_dir(parsed_url.path):
+            if not path_is_dir(parsed_url.path):
                 file_path = _retrieve_file_using_urllib2(parsed_url, download_path)
             else:
                 raise NotImplementedError
@@ -27,17 +28,13 @@ def retrieve_resources(track_resources, download_path):
     return fetched_paths
 
 
-def _path_is_dir(path):
-    return path[-1] == '/'
-
-
 def _retrieve_file_using_urllib2(parsed_url, dest_dir):
     dest_dir = dest_dir if dest_dir[-1] is not '/' else dest_dir[:-1]
 
     filename = dest_dir + '/' + parsed_url.path.split('/')[-1]
 
     if os.path.exists(filename):
-        filename = _get_new_filename(filename)
+        filename = get_new_enumerated_filename(filename)
     try:
         open_url = urllib2.urlopen(parsed_url.geturl())
     except urllib2.HTTPError, e:
@@ -69,26 +66,3 @@ def _retrieve_file_using_urllib2(parsed_url, dest_dir):
     h5_file.close()
     return filename
 
-
-def _get_new_filename(filename):
-    filename_no_ext, file_ext = _custom_splitext(filename)
-    trailing_digits = re.compile('.+(\(\d+\))$')
-    match = re.match(trailing_digits, filename_no_ext)
-    if match:
-        group = match.groups()[0]
-        new_number = str(int(group[1:-1]) + 1)
-        filename = filename_no_ext[:-len(group)] + '(' + new_number + ')' + file_ext
-    else:
-        filename = filename_no_ext + '(1)' + file_ext
-    return filename
-
-
-def _custom_splitext(filename):
-    cur_filename_no_ext, cur_file_ext = os.path.splitext(filename)
-    while True:
-        filename_no_ext, file_ext = os.path.splitext(cur_filename_no_ext)
-        if file_ext == '':
-            return cur_filename_no_ext, cur_file_ext
-        else:
-            cur_filename_no_ext = filename_no_ext
-            cur_file_ext = file_ext + cur_file_ext
