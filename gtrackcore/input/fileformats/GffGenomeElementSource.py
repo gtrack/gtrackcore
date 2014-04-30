8669b83cbb9dcd51753fbfd34084cfb0a7ba9286
Sveinung Gundersen <sveinugu@gmail.com>
2014-04-30 18:34:33 +0200

    Added support for ##FASTA (ignoring the sequences)

--- gtrackcore/input/fileformats/GffGenomeElementSource.py
+++ gtrackcore/input/fileformats/GffGenomeElementSource.py
@@ -10,20 +10,23 @@ class GffGenomeElementSource(GenomeElementSource):
     FILE_SUFFIXES = ['gff', 'gff3']
     FILE_FORMAT_NAME = 'GFF'
     _numHeaderLines = 0
-    
+
     _inputIsOneIndexed = True
     _inputIsEndInclusive = True
-    
+
     def __new__(cls, *args, **kwArgs):
         return object.__new__(cls)
-    
+
     def _next(self, line):
+        if line.startswith('##FASTA'):
+            raise StopIteration
+
         if len(line)>0 and line[0]=='#':
             return None
-        
+
         origCols = line.split('\t')
         cols = [unquote(x) for x in origCols]
-        
+
         if len(cols) != 9:
             raise InvalidFormatError("Error: GFF files must contain 9 tab-separated columns")
 
@@ -37,7 +40,7 @@ class GffGenomeElementSource(GenomeElementSource):
         ge.strand = self._getStrandFromString(cols[6])
         ge.phase = cols[7]
         ge.attributes = cols[8]
-        
+
         for attr in origCols[8].split(';'):
             attrSplitted = attr.split('=')
             if len(attrSplitted) == 2:
@@ -46,7 +49,7 @@ class GffGenomeElementSource(GenomeElementSource):
                     ge.id = unquote(val)
                 elif key.lower() == 'name':
                     ge.name = unquote(val)
-                
+
         return ge
 
     @classmethod
