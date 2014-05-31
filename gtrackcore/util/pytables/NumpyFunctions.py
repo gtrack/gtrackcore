534e26e40025482431ffaf97d480b64131b8ccf9
Brynjar Rongved <brynjar.rongved@gmail.com>
2014-05-31 14:09:21 +0200

    numpy.empty is faster than numpy.zeros. The array is filled with default values, so the zeros are overwritten anyway

--- gtrackcore/util/pytables/NumpyFunctions.py
+++ gtrackcore/util/pytables/NumpyFunctions.py
@@ -3,7 +3,7 @@ from gtrackcore.util.CommonConstants import BINARY_MISSING_VAL
 
 
 def insert_into_array_of_larger_shape(array, shape):
-    new_array = numpy.zeros(shape=shape, dtype=array.dtype)
+    new_array = numpy.empty(shape=shape, dtype=array.dtype)
     new_array.fill(get_default_numpy_value(array.dtype))
     new_array[[slice(0, shape_dimension) for shape_dimension in array.shape]] = array
     return new_array
