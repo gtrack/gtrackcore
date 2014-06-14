0b4d3a7a1544b1ee23d3d6d3e10f8bd746f6b2f9
Sveinung Gundersen <sveinugu@gmail.com>
2014-06-14 11:42:19 +0300

    Now able to compare GenomeElements with GenomeRegions

--- gtrackcore/input/core/GenomeElement.py
+++ gtrackcore/input/core/GenomeElement.py
@@ -20,12 +20,12 @@ class GenomeElement(GenomeRegion):
                 end += genomeAnchor.start
         else:
             chr = str(genomeAnchor)
-        
+
         return GenomeElement(genome, chr, start, end, trackEl.val(), trackEl.strand(), \
                              id=trackEl.id(), edges=edges, weights=weights, \
                              extra=dict([(key, getattr(trackEl, key)()) for key in trackEl.getAllExtraKeysInOrder()]), \
                              orderedExtraKeys=trackEl.getAllExtraKeysInOrder())
-        
+
     def __init__(self, genome=None, chr=None, start=None, end=None, val=None, strand=None, id=None, edges=None, weights=None, extra=None, orderedExtraKeys=None, isBlankElement=False, **kwArgs):
         # __dict__ is used for speedup, so that __setattr__ is not called
         members = self.__dict__
@@ -39,7 +39,7 @@ class GenomeElement(GenomeRegion):
         members['edges'] = edges #ids of linked elements (list of strings)
         members['weights'] = weights #resp. weights of edges (list of values, using similar types as for 'value' above)
         members['isBlankElement'] = isBlankElement
-        
+
         if extra is None:
             members['orderedExtraKeys'] = [] #keys in extra dict in correct order. Is used instead of OrderedDict because of performance issues
             members['extra'] = {}
@@ -49,11 +49,11 @@ class GenomeElement(GenomeRegion):
             else:
                 members['orderedExtraKeys'] = orderedExtraKeys
             members['extra'] = dict(extra) #dict of extra columns, from column name (str) -> contents (str)
-        
+
         for kw in kwArgs:
             members['orderedExtraKeys'].append(kw)
             members['extra'][kw] = kwArgs[kw]
-            
+
     def getCopy(self):
         extraCopy = dict(self.extra)
         orderedExtraKeysCopy = list(self.orderedExtraKeys)
@@ -64,7 +64,7 @@ class GenomeElement(GenomeRegion):
             return self.__dict__['extra'][item]
         except KeyError:
             raise AttributeError
-            
+
     def __setattr__(self, item, value):
         if item not in self.__dict__ and 'extra' in self.__dict__:
             if item not in self.__dict__['extra']:
@@ -75,17 +75,17 @@ class GenomeElement(GenomeRegion):
 
     def __str__(self):
         #return self.toStr()
-        
+
         #self.start+1 because we want to show 1-indexed, end inclusive output
         return (str(self.chr) + ':' if not self.chr is None else '')\
             + (str(self.start+1) if not self.start is None else '')\
             + ('-' + str(self.end) if not self.end is None else '')\
             + ((' (Pos)' if self.strand else ' (Neg)') if not self.strand in [None, BINARY_MISSING_VAL] else '')\
             + ((' [' + str(self.val) + ']') if self.val is not None else '')
-    
+
     def __repr__(self):
         return str(self)
-    
+
     def toStr(self):
         #self.start+1 because we want to show 1-indexed, end inclusive output
         return (str(self.genome) + ':' if not self.genome is None else '')\
@@ -98,17 +98,21 @@ class GenomeElement(GenomeRegion):
             + ((' edges="%s"' % str(self.edges)) if self.edges != None else '')\
             + ((' weights="%s"' % str(self.weights)) if self.weights != None else '')\
             + ((' extra="%s"' % str(self.extra)) if self.extra != {} else '')
-            
-    def __cmp__(self, other):    
+
+    def __cmp__(self, other):
         if other is None:
             return -1
         else:
             #print self.toStr()
             #print other.toStr()
             #print [cmp(getattr(self, attr), getattr(other, attr)) for attr in ['genome','chr','start','end','val','strand','id','edges','weights','extra']]
-            return cmp([self.genome, self.chr, self.start, self.end, self.val, self.strand, self.id, self.edges, self.weights, self.extra] , \
-                [other.genome, other.chr, other.start, other.end, other.val, other.strand, other.id, other.edges, other.weights, other.extra])
-    
+            try:
+                return cmp([self.genome, self.chr, self.start, self.end, self.val, self.strand, self.id, self.edges, self.weights, self.extra] , \
+                    [other.genome, other.chr, other.start, other.end, other.val, other.strand, other.id, other.edges, other.weights, other.extra])
+            except:
+                if isinstance(other, GenomeRegion):
+                    return GenomeRegion.__cmp__(self, other)
+
     def overlaps(self, other):
         assert all((getattr(self, attr) is None) == (getattr(other, attr) is None) \
                    for attr in ['genome', 'chr', 'start', 'end'])
@@ -118,18 +122,18 @@ class GenomeElement(GenomeRegion):
         if self.genome is not None:
             if self.genome != other.genome:
                 return False
-        
+
         if self.chr != other.chr:
             return False
-        
+
         if not None in [self.start, self.end]:
             return False if self.start >= other.end or self.end <= other.start else True
         else:
             return True if (self.start is not None and self.start == other.start) or \
                             (self.end is not None and self.end == other.end) else False
-    
+
     def reprIsDense(self):
         return self.start is None and self.end is None
-    
+
     def validAsRegion(self):
         return not None in [self.genome, self.chr, self.start, self.end]
