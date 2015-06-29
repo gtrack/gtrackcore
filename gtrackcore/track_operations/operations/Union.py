6a4f4f2c292fdd347a1cdee2c5cbbf14116e7014
Sivert Hatteberg <sivertkh@ifi.uio.no>
2015-06-29 22:07:37 +0200

    cleaning up some git stuff

--- gtrackcore/track_operations/operations/Union.py
+++ gtrackcore/track_operations/operations/Union.py
@@ -2,8 +2,22 @@ __author__ = 'skh'
 
 
 from gtrackcore.track_operations.operations import Operator
+from gtrackcore.track_operations.TrackContents import TrackContents
+from gtrackcore.track.core.TrackView import TrackView
+from gtrackcore.track.format.TrackFormat import TrackFormatReq
+
 
 class Union(Operator):
+    _NUM_TRACKS = 2
+    _TRACK_REQUIREMENTS = [TrackFormatReq(dense=False, allowOverlaps=False),
+                           TrackFormatReq(dense=False, allowOverlaps=False)]
+    _RESULT_ALLOW_OVERLAPS = False
 
-    def compute(self):
-        return
+    def _call(self, region, trackView1, trackView):
+        starts1 = trackView1.startsAsNumpyArray()
+
+
+
+        returnTv = self._createTrackView(region, starts, ends)
+        return returnTv
+
