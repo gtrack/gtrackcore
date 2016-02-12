3806ebb71b4dd8cbe10be8be70977ab264165da3
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-02-12 15:53:24 +0100

    New exception used by the operations.

---
+++ gtrackcore/track_operations/exeptions/Operations.py
@@ -0,0 +1,12 @@
+__author__ = 'skh'
+
+
+class OutputTrackTypeNotSupportedError(Exception):
+    """
+    Exception used when the a operation do not support the given output
+    TrackFormat.
+    """
+
+    def __init__(self, trackType, operation):
+        self.message = "Track type {0} not supported by operation {" \
+                       "1}".format(trackType, operation)
