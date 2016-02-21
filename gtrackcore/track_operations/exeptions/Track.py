b6b146a6824617b3fd0249a63786011592d6c8d6
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-02-21 21:57:01 +0100

    New TrackContents exception

--- gtrackcore/track_operations/exeptions/Track.py
+++ gtrackcore/track_operations/exeptions/Track.py
@@ -7,3 +7,12 @@ class TrackIncompatibleException(Exception):
 
     def __init__(self, message):
         self.message = message
+
+
+class TrackContentsEmptyError(Exception):
+    """
+    Used when calling methods on a empty Track Contents object.
+    """
+
+    def __init__(self):
+        self.message = "Track Content is empty!"
