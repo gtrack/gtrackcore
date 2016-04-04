9d6a0d1ff6941c353d7a702a256de872b0170bcc
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-04-04 22:24:46 +0200

    Implemented classmethods and updated to new style

--- gtrackcore/track_operations/operations/CountElements.py
+++ gtrackcore/track_operations/operations/CountElements.py
@@ -4,8 +4,11 @@ from gtrackcore.track_operations.operations.Operator import Operator
 from gtrackcore.track_operations.TrackContents import TrackContents
 from gtrackcore.track.core.TrackView import TrackView
 from gtrackcore.track.format.TrackFormat import TrackFormatReq
-from gtrackcore.track_operations.raw_operations.CountElements import countElements
-
+from gtrackcore.track_operations.raw_operations.CountElements import \
+    countElements
+from gtrackcore.track_operations.Genome import Genome
+from gtrackcore.track_operations.utils.TrackHandling import \
+    createTrackContentFromFile
 
 class CountElements(Operator):
     _NUM_TRACKS = 1
@@ -21,3 +24,32 @@ class CountElements(Operator):
         nr = countElements(starts, ends)
 
         return nr
+
+    @classmethod
+    def createSubParser(cls, subparsers):
+        """
+        Creates a subparser. Used by GTool
+        :param subparsers:
+        :return: None
+        """
+        parser = subparsers.add_parser('countElemts', help='Find the nr of '
+                                                           'elements in a '
+                                                           'track')
+        parser.add_argument('track', help='File path of track')
+        parser.add_argument('genome', help='File path of Genome definition')
+        parser.set_defaults(which='Count')
+
+    @classmethod
+    def createOperation(cls, args):
+        """
+        Generator classmethod used by GTool
+
+        :param args: args from GTool
+        :return: Intersect object
+        """
+        genome = Genome.createFromJson(args.genome)
+
+        track = createTrackContentFromFile(genome, args.trackA,
+                                            args.allowOverlap)
+
+        return CountElements(track)
