4bad60b5aedf8b512020981f814a641db0f4ce9f
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-09-22 22:22:50 +0200

    Test completed for complement

--- gtrackcore/track_operations/operations/Complement.py
+++ gtrackcore/track_operations/operations/Complement.py
@@ -23,6 +23,29 @@ class Complement(Operator):
     Creates a complementing track.
     """
 
+    def __init__(self, *args, **kwargs):
+        self._kwargs = kwargs
+        self._options = {'debug': False,
+                         'trackFormatReqChangeable': False,
+                         'resultTrackFormatReqChangeable': False
+                         }
+
+        self._tracks = args
+        self._trackFormat = args[0]
+
+        self._numTracks = 1
+        self._resultIsTrack = True
+
+        self._trackRequirements = \
+            [TrackFormatReq(dense=False, allowOverlaps=False)]
+
+        self._resultTrackRequirements = TrackFormatReq(dense=False,
+                                                       allowOverlaps=False,
+                                                       val=False,
+                                                       linked=False)
+
+        super(self.__class__, self).__init__(*args, **kwargs)
+
     def _calculate(self, region, tv):
         logging.debug("Start call! region:{0}".format(region))
 
@@ -46,66 +69,6 @@ class Complement(Operator):
         else:
             return None
 
-    def _setConfig(self, track):
-        # None changeable properties
-        self._numTracks = 1
-        self._trackRequirements = \
-            [TrackFormatReq(dense=False, allowOverlaps=False)]
-        self._resultIsTrack = True
-
-        # Set defaults for changeable properties
-        self._allowOverlap = False
-        self._resultAllowOverlaps = False
-
-        self._useFraction = False
-        self._useMissingStrands = False
-        self._treatMissingAsPositive = True
-        # For now the result track is always of the same type as track A
-        # TODO: Solve this for the case where A and b are not of the same type.
-        self._resultTrackRequirements = self._trackRequirements[0]
-
-    def _parseKwargs(self, **kwargs):
-        """
-        :param kwargs:
-        :return: None
-        """
-
-        if 'allowOverlap' in kwargs:
-            self._allowOverlap = kwargs['allowOverlap']
-        else:
-            self._allowOverlap = False
-
-        if 'debug' in kwargs:
-            self._debug = kwargs['debug']
-        else:
-            self._debug = False
-
-        if self._debug:
-            level = logging.DEBUG
-        else:
-            level = logging.INFO
-        logging.basicConfig(stream=sys.stderr, level=level)
-
-    def _updateTrackFormat(self):
-        """
-        If we enable or disable overlapping tracks as input, we need to
-        update the track requirement as well.
-        :return: None
-        """
-        if self._allowOverlap:
-            self._trackRequirements = \
-                [TrackFormatReq(dense=False, allowOverlaps=True)]
-        else:
-            self._trackRequirements = \
-                [TrackFormatReq(dense=False, allowOverlaps=False)]
-
-    def _updateResultTrackFormat(self):
-        """
-        There can not be overlap in the result.
-        :return: None
-        """
-        self._resultTrackRequirements = \
-            [TrackFormatReq(dense=False, allowOverlaps=False)]
 
     def preCalculation(self, track):
         return track
@@ -128,32 +91,6 @@ class Complement(Operator):
         parser.add_argument('--allowOverlap', action='store_true',
                             help="Allow overlap in the input track")
         parser.set_defaults(which='Complement')
-
-    @classmethod
-    def createOperation(cls, args):
-        """
-        Generator classmethod used by GTool
-
-        :param args: args from GTool
-        :return: Intersect object
-        """
-        genome = Genome.createFromJson(args.genome)
-
-        track = createTrackContentFromFile(genome, args.track,
-                                           args.allowOverlap)
-
-        allowOverlap = args.allowOverlap
-        # TODO: use overlap...
-
-        return Complement(track, allowOverlap=allowOverlap)
-
-    @classmethod
-    def createTrackName(cls):
-        """
-        Track name used by GTools when saving the track i GTrackCore
-        :return: Generated track name as a string
-        """
-        return "complement-{0}".format(int(time.time()))
 
     def printResult(self):
         """
