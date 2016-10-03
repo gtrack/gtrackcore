a1f8aa5532b38f5878437ddc4eb3772d7772e526
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-10-04 00:48:02 +0200

    More comprehensive testing of the coverage operation

--- gtrackcore/test/track_operations/operations/CoverageTest.py
+++ gtrackcore/test/track_operations/operations/CoverageTest.py
@@ -27,45 +27,76 @@ class CoverageTest(unittest.TestCase):
         self.chromosomes = (GenomeRegion('hg19', c, 0, l)
                             for c, l in GenomeInfo.GENOMES['hg19']['size'].iteritems())
 
-    def _runCoverageTest(self, starts, ends, expCover):
-        """
+    def _runTest(self, starts=None, ends=None, strands=None, values=None,
+                 ids=None, edges=None, weights=None, expCoverage=None,
+                 customAverageFunction=None, customChrLength=None,
+                 allowOverlap=True, debug=False):
 
-        :param starts:
-        :param ends:
-        :param expCover:
+        track = createSimpleTestTrackContent(startList=starts, endList=ends,
+                                             valList=values,
+                                             strandList=strands,
+                                             idList=ids, edgeList=edges,
+                                             weightsList=weights,
+                                             customChrLength=customChrLength)
+
+        c = Coverage(track, debug=debug)
+
+        self.assertTrue((c is not None))
+        result = c.calculate()
+        self.assertTrue(result is not None)
+
+        resFound = False
+
+        for (k, v) in result.iteritems():
+            if cmp(k, self.chr1) == 0 or cmp(k, self.chr1Small) == 0:
+                # All test tracks are in chr1
+                self.assertTrue(v == expCoverage)
+                resFound = True
+
+            else:
+                # Tests if all tracks no in chr1 have a size of 0.
+                self.assertEqual(v.size, 0)
+
+        self.assertTrue(resFound)
+
+    def testPointsSimple(self):
+        """
+        Test coverage of simple points track
         :return:
         """
 
-        track = createSimpleTestTrackContent(startList=starts, endList=ends)
-        u = Coverage(track)
-        res = u()
+        self._runTest(starts=[4,10], expCoverage=2)
 
-        for (k, v) in res.iteritems():
-            if cmp(k, self.chr1) == 0:
-                self.assertTrue(v, expCover)
-
-    def testSimpleCoverage(self):
+    def testPointsComplex(self):
         """
-        Simple coverage
+        Test coverage of more complex points track
+        :return:
+        """
+        starts = [4,10,50,73,123,21441,4124]
+
+        self._runTest(starts=starts, expCoverage=len(starts))
+
+    def testSegmentsSimple(self):
+        """
+        Test coverage of simple segment track
         :return: None
         """
-        self._runCoverageTest(starts=[2], ends=[4], expCover=2)
+        self._runTest(starts=[2], ends=[4], expCoverage=2)
 
-    def testComplexCoverage(self):
+    def testSegmentsComplex(self):
         """
         A more complex coverage
         :return: None
         """
-        self._runCoverageTest(starts=[2,10,100], ends=[4,45,150],
-                              expCover=2+35+50)
+        self._runTest(starts=[2,10,100], ends=[4,45,150],
+                              expCoverage=2+35+50)
 
-    def testOverlapCoverage(self):
+    def testSegmentsOverlapping(self):
         """
         Coverage of overlapping segments
         :return: None
         """
-        self._runCoverageTest(starts=[2,6], ends=[6,8], expCover=6)
-
+        self._runTest(starts=[2,6], ends=[6,8], expCoverage=6)
 
 if __name__ == "__main__":
     unittest.main()
