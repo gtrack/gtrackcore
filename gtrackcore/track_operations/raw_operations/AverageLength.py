c95812af016e1c8730fcf39cd9bafdb2e073938e
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-10-03 20:16:48 +0200

    Added support for a custom average function

--- gtrackcore/track_operations/raw_operations/AverageLength.py
+++ gtrackcore/track_operations/raw_operations/AverageLength.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-def averageLength(starts, ends):
+def averageLength(starts, ends, customAverageFunction=None, deug=False):
     """
     Find the average length of the segments in a track.
     Possible extension: Support for weights.
@@ -10,7 +10,15 @@ def averageLength(starts, ends):
     :param ends: Numpy array. Ends
     :return: The average length of segments in a track.
     """
+
+    if customAverageFunction is None:
+        averageFunction = np.average
+    else:
+        if deug:
+            print("averageLength: Using a custom average!")
+        averageFunction = customAverageFunction
+
     # Find the length of each segment.
     length = ends - starts
 
-    return np.average(length)
+    return averageFunction(length)
