2aab5cf241094f48a90f153efcd68bc21c9a78de
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-10-17 17:37:27 +0200

    Updated to new kwarg style, removed support for partitions

--- gtrackcore/track_operations/operations/CountElements.py
+++ gtrackcore/track_operations/operations/CountElements.py
@@ -1,74 +1,37 @@
-__author__ = 'skh'
+from collections import OrderedDict
 
-from gtrackcore.track_operations.operations.Operator import Operator
-from gtrackcore.track_operations.TrackContents import TrackContents
-from gtrackcore.track.core.TrackView import TrackView
 from gtrackcore.track.format.TrackFormat import TrackFormatReq
+
 from gtrackcore.track_operations.raw_operations.CountElements import \
     countElements
-from gtrackcore.track_operations.Genome import Genome
-from gtrackcore.track_operations.utils.TrackHandling import \
-    createTrackContentFromFile
+
+from gtrackcore.track_operations.operations.Operator import Operator
+from gtrackcore.track_operations.operations.Operator import KwArgumentInfo
 
 class CountElements(Operator):
 
-    def __init__(self, *args, **kwargs):
-        assert len(args) == 1
-
-        self._kwargs = kwargs
-        self._options = {'debug': False,
-                         'allowOverlap': True,
-                         'resultAllowOverlap': False,
-                         }
-
-        # Save the tracks
-        self._tracks = args
-        self._trackFormat = args[0].trackFormat
-
-        # None changeable properties
-        self._numTracks = 1
-        self._resultIsTrack = False
-
-        self._trackRequirements = [[TrackFormatReq(dense=False),
-                                   TrackFormatReq(dense=True, interval=True)]]
-        self._resultTrackRequirements = None
-
-        super(self.__class__, self).__init__(*args, **kwargs)
+    _trackHelpList = ['Track to be count elements on']
+    _numTracks = 1
+    _resultIsTrack = False
+    _trackRequirements = [TrackFormatReq(dense=False)]
+    _resultTrackFormat = None
 
     def _calculate(self, region, tv):
         starts = tv.startsAsNumpyArray()
         ends = tv.endsAsNumpyArray()
 
         nr = countElements(starts, ends)
-
         return nr
 
-    def preCalculation(self, track):
-        return track
-
-    def postCalculation(self, result):
-        return result
-
-    @classmethod
-    def createSubParser(cls, subparsers):
-        """
-        Creates a subparser. Used by GTool
-        :param subparsers:
-        :return: None
-        """
-        parser = subparsers.add_parser('countElements',
-                                       help='Find the nr of elements in a '
-                                            'track')
-        parser.add_argument('track', help='File path of track')
-        parser.add_argument('genome', help='File path of Genome definition')
-        parser.set_defaults(which='Count')
-
-    def printResult(self):
-        """
-        Print the results if calculation is finished.
-        :return:
-        """
-        if self._resultFound:
-            print(self._out)
-        else:
-            print("ERROR! Calculation not run!")
+    def _getKwArgumentInfoDict(self):
+        return OrderedDict([
+            ('debug',
+             KwArgumentInfo('debug', 'd', 'Print debug info', bool, False)),
+            ('resultAllowOverlap',
+             KwArgumentInfo('resultAllowOverlap','o',
+                            'Allow overlap in the result track.', bool,
+                            False)),
+            ('total',
+             KwArgumentInfo('total', 't',
+                            'Sum the coverage for all regions',
+                            bool, False))])
