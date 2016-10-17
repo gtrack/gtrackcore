fb16d70ff04e0510a2d410a44ca0b1db887f46d5
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-10-18 01:06:23 +0200

    New exception

--- gtrackcore/track_operations/exeptions/Operations.py
+++ gtrackcore/track_operations/exeptions/Operations.py
@@ -10,3 +10,6 @@ class OutputTrackTypeNotSupportedError(Exception):
     def __init__(self, trackType, operation):
         self.message = "Track type {0} not supported by operation {" \
                        "1}".format(trackType, operation)
+
+class InvalidArgumentError(Exception):
+    pass
