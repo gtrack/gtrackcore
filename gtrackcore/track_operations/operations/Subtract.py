2ece98919129681bc2845b1d8d919a7cbbe53893
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-10-17 23:28:25 +0200

    Updated to use the new kwarg style

--- gtrackcore/track_operations/operations/Subtract.py
+++ gtrackcore/track_operations/operations/Subtract.py
@@ -1,47 +1,27 @@
 
-import time
+from collections import OrderedDict
 
 from gtrackcore.track.format.TrackFormat import TrackFormatReq
 
-from gtrackcore.track_operations.operations.Operator import Operator
 from gtrackcore.track_operations.raw_operations.Subtract import subtract
+
+from gtrackcore.track_operations.operations.Operator import Operator
+from gtrackcore.track_operations.operations.Operator import KwArgumentInfo
 
 from gtrackcore.track_operations.utils.TrackHandling import \
     createRawResultTrackView
-from gtrackcore.track_operations.utils.TrackHandling import \
-    createTrackContentFromFile
 
-from gtrackcore.track_operations.Genome import Genome
+from gtrackcore.track_operations.operations.Merge import Merge
+from gtrackcore.track_operations.operations.RemoveDeadLinks import \
+    RemoveDeadLinks
 
 class Subtract(Operator):
 
-    def __init__(self, *args, **kwargs):
-        self._kwargs = kwargs
-        self._options = {'debug': False,
-                         'allowOverlaps': False,
-                         'resultAllowOverlaps': False,
-                         'useStrands': True,
-                         'treatMissingAsNegative': False
-                         }
-
-        # Save the tracks
-        self._tracks = args
-
-        # As we subtract track 2 from track 1, the track format
-        # of the result is equal to track 1
-        self._trackFormat = args[0].trackFormat
-
-        # Core properties
-        self._numTracks = 2
-        self._resultIsTrack = True
-        self._trackRequirements = \
-            [TrackFormatReq(dense=False, allowOverlaps=False),
-             TrackFormatReq(dense=False, allowOverlaps=False)]
-
-        self._resultTrackRequirements = TrackFormatReq(
-            name=self._trackFormat.getFormatName())
-
-        super(self.__class__, self).__init__(*args, **kwargs)
+    _trackHelpList = ['Base track', 'Track to be subtracted from']
+    _numTracks = 2
+    _resultIsTrack = True
+    _trackRequirements = [TrackFormatReq(dense=False),
+                          TrackFormatReq(dense=False)]
 
     def _calculate(self, region, tv1, tv2):
 
@@ -77,66 +57,53 @@ class Subtract(Operator):
             ends = ret[1]
             index = ret[2]
 
-            if self._debug:
-                print("Return from raw!")
-                print("starts: {}".format(starts))
-                print("ends:   {}".format(ends))
-                print("index:  {}".format(index))
-
             return createRawResultTrackView(index, region, tv1,
-                                            self.resultAllowOverlaps,
+                                            self._resultAllowOverlap,
                                             newStarts=starts, newEnds=ends,
-                                            trackFormatReq=self._resultTrackRequirements)
+                                            trackFormat=self._resultTrackFormat)
         else:
             return None
 
-    def preCalculation(self, track):
-        # Merge inputs
-        return track
+    def _preCalculation(self):
+        # There cant be any overlap in any of the tracks.
+        m1 = Merge(self._tracks[0], useStrands=self._useStrands,
+                   treatMissingAsNegative=self._treatMissingAsNegative)
+        t1 = m1.calculate()
 
-    def postCalculation(self, result):
-        #remove dead links
-        return result
+        m2 = Merge(self._tracks[1], useStrands=self._useStrands,
+                   treatMissingAsNegative=self._treatMissingAsNegative)
+        t2 = m2.calculate()
 
-    @classmethod
-    def createSubParser(cls, subparsers):
+        self._tracks = [t1, t2]
+
+    def _postCalculation(self):
+        # Fix split links
+
+        if self._result.trackFormat.isLinked():
+            r = RemoveDeadLinks(self._result)
+            self._result = r.calculate()
+
+    def _setResultTrackFormat(self):
         """
-        Creates a subparser. Used by GTool
-        :param subparsers:
-        :return: None
-        """
-        parser = subparsers.add_parser('subtract', help='Subtract track B '
-                                                        'from track A')
-        parser.add_argument('trackA', help='File path of track A')
-        parser.add_argument('trackB', help='File path of track B')
-        parser.add_argument('genome', help='File path of Genome definition')
-        parser.add_argument('--allowOverlap', action='store_true',
-                            help="Allow overlap in the resulting track")
-        parser.set_defaults(which='Subtract')
-
-    @classmethod
-    def createOperation(cls, args):
-        """
-        Generator classmethod used by GTool
-
-        :param args: args from GTool
-        :return: Intersect object
-        """
-        genome = Genome.createFromJson(args.genome)
-
-        trackA = createTrackContentFromFile(genome, args.trackA,
-                                            args.allowOverlap)
-        trackB = createTrackContentFromFile(genome, args.trackB,
-                                            args.allowOverlap)
-
-        allowOverlap = args.allowOverlap
-        # TODO: use overlap...
-
-        return Subtract(trackA, trackB)
-
-    def printResult(self):
-        """
-        Result is a track. Do nothing
+        Create the correct TrackFormat for the output track.
         :return:
         """
-        pass
+        # We are not changing the type of base track. Using its TrackFormat.
+        self._resultTrackFormat = self._tracks[0].trackFormat
+
+    def _getKwArgumentInfoDict(self):
+        return OrderedDict([
+            ('debug',
+             KwArgumentInfo('debug', 'd', 'Print debug info', bool, False)),
+            ('resultAllowOverlap',
+             KwArgumentInfo('resultAllowOverlap', 'o',
+                            'Allow overlap in the result track.', bool,
+                            False)),
+            ('useStrands',
+             KwArgumentInfo('useStrands', 's', 'Follow the strand direction',
+                            bool, True)),
+            ('treatMissingAsNegative',
+             KwArgumentInfo('treatMissingAsNegative', 'n',
+                            'Treat any missing strand as if they are '
+                            'negative. The default is to treat them as positive',
+                            bool, False))])
