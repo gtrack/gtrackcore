3ac82a1a5c6792953de705016375f0c72006a84b
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-10-17 18:12:19 +0200

    Updated to use the new kwarg style

--- gtrackcore/track_operations/operations/AverageLength.py
+++ gtrackcore/track_operations/operations/AverageLength.py
@@ -1,72 +1,32 @@
 
-from gtrackcore.track_operations.operations.Operator import Operator
-from gtrackcore.track_operations.TrackContents import TrackContents
-from gtrackcore.track.core.TrackView import TrackView
+from collections import OrderedDict
+
 from gtrackcore.track.format.TrackFormat import TrackFormatReq
+
 from gtrackcore.track_operations.raw_operations.AverageLength import \
     averageLength
-from gtrackcore.track_operations.Genome import Genome
-from gtrackcore.track_operations.utils.TrackHandling import \
-    createTrackContentFromFile
+
+from gtrackcore.track_operations.operations.Operator import Operator
+from gtrackcore.track_operations.operations.Operator import KwArgumentInfo
 
 class AverageLength(Operator):
 
-    def __init__(self, *args, **kwargs):
-        self._kwargs = kwargs
-        self._options = {'debug': False,
-                         'allowOverlap': True,
-                         'customAverageFunction': None
-                         }
-        # Save the tracks
-        self._tracks = args
-
-        # Operations core requirements
-        self._numTracks = 1
-
-        self._trackFormat = args[0].trackFormat
-
-        self._trackRequirements = [[TrackFormatReq(dense=False),
-                                    TrackFormatReq(dense=True, interval=True)]]
-        self._resultIsTrack = False
-        self._resultTrackRequirements = None
-
-        super(self.__class__, self).__init__(*args, **kwargs)
+    _trackHelpList = ['Track to find the average element length on']
+    _numTracks = 1
+    _resultIsTrack = False
+    _trackRequirements = [TrackFormatReq(dense=False)]
+    _resultTrackFormat = None
 
     def _calculate(self, region, tv):
-
         starts = tv.startsAsNumpyArray()
         ends = tv.endsAsNumpyArray()
-
         nr = averageLength(starts, ends, self._customAverageFunction)
-
         return nr
 
-    def preCalculation(self, track):
-        return track
-
-    def postCalculation(self, result):
-        return result
-
-    @classmethod
-    def createSubParser(cls, subparsers):
-        """
-        Creates a subparser. Used by GTool
-        :param subparsers:
-        :return: None
-        """
-        parser = subparsers.add_parser('averageLength',
-                                       help='Find the average length of the '
-                                            'elements in a track')
-        parser.add_argument('track', help='File path of track')
-        parser.add_argument('genome', help='File path of Genome definition')
-        parser.set_defaults(which='Count')
-
-    def printResult(self):
-        """
-        :return:
-        """
-
-        if self._resultFound:
-            print(self._out)
-        else:
-            print("ERROR! Calculation not run!")
+    def _getKwArgumentInfoDict(self):
+        return OrderedDict([
+            ('debug',
+             KwArgumentInfo('debug', 'd', 'Print debug info', bool, False)),
+            ('customAverageFunction',
+             KwArgumentInfo('customAverageFunction','c',
+                            'Use a custom average function', None, None))])
