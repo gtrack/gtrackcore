4ff3b96af0199b1afa742b7ad643dcc37315cc6b
Sivert Hatteberg <sivertkh@ifi.uio.no>
2016-10-17 14:29:56 +0200

    We no define the kwarguments by using namedtuples. The __init__ method and the generate subparser methods are removed. Subparser will be generated by using the named tuples.

--- gtrackcore/track_operations/operations/Flank.py
+++ gtrackcore/track_operations/operations/Flank.py
@@ -1,22 +1,19 @@
 
 import logging
-import sys
-import time
+
+from collections import OrderedDict
+from collections import namedtuple
 
 from gtrackcore.track.core.TrackView import TrackView
-from gtrackcore.track.format.TrackFormat import TrackFormatReq
 from gtrackcore.track.format.TrackFormat import TrackFormat
+from gtrackcore.track.format.TrackFormat import TrackFormatReq
 
 from gtrackcore.track_operations.operations.Operator import Operator
+from gtrackcore.track_operations.operations.Operator import KwArgumentInfo
+
 from gtrackcore.track_operations.operations.Merge import Merge
-from gtrackcore.track_operations.TrackContents import TrackContents
-from gtrackcore.track_operations.RawOperationContent import RawOperationContent
-from gtrackcore.track_operations.utils.TrackHandling import \
-    createTrackContentFromFile
-from gtrackcore.track_operations.Genome import Genome
 from gtrackcore.track_operations.utils.TrackHandling import \
     createRawResultTrackView
-
 
 from gtrackcore.track_operations.raw_operations.Flank import flank
 
@@ -24,37 +21,10 @@ class Flank(Operator):
     """
     Creates a new track of flanking segments.
     """
-
-    def __init__(self, *args, **kwargs):
-        assert args[0] is not None
-        self._kwargs = kwargs
-        self._options = {'debug': False,
-                         'allowOverlaps': False,
-                         'resultAllowOverlaps': False,
-                         'both': None,
-                         'start': None,
-                         'end': None,
-                         'useFraction': False,
-                         'useStrands': True,
-                         'treatMissingAsNegative': False
-                         }
-        # Save the tracks
-        self._tracks = args[0]
-        self._trackFormat = self._tracks.trackFormat
-
-        # Core properties
-        self._numTracks = 1
-        self._resultIsTrack = True
-
-        # Input need to be a none dense track
-        self._trackRequirements = \
-            [TrackFormatReq(dense=False, allowOverlaps=self._allowOverlaps)]
-
-        # TrackResultReq is always a segment type
-        self._resultTrackRequirements = \
-            TrackFormatReq(dense=False, allowOverlaps=self._resultAllowOverlaps)
-
-        super(self.__class__, self).__init__(*args, **kwargs)
+    _trackHelpList = ['Track to create flank track from']
+    _numTracks = 1
+    _resultIsTrack = True
+    _trackRequirements = [TrackFormatReq(dense=False)]
 
     def _calculate(self, region, tv):
         # Remove RawOperationsContent
@@ -72,8 +42,8 @@ class Flank(Operator):
         regionSize = len(region)
 
         ret = flank(starts, ends, regionSize, strands=strands,
-                    start=self._start, end=self._end, both=self._both,
-                    useStrands=self._useStrands,
+                    downstream=self._downstream, upstream=self._upstream,
+                    both=self._both, useStrands=self._useStrands,
                     useFraction=self._useFraction,
                     treatMissingAsNegative=self._treatMissingAsNegative,
                     debug=self._debug)
@@ -95,57 +65,53 @@ class Flank(Operator):
         else:
             return None
 
-    def preCalculation(self, track):
-        return track
-
-    def postCalculation(self, track):
+    def _postCalculation(self):
         if not self._resultAllowOverlap:
+            track = self._result
             # Overlap not allowed in the result. Using merge to remove it
-            m = Merge(track, both=True, mergeValues=True,
-                      useStrands=self._useStrands,
+            m = Merge(track, mergeValues=True, useStrands=self._useStrands,
                       mergeLinks=True, allowOverlap=False)
             res = m.calculate()
-            return res
-        else:
-            return track
+            self._result = res
 
-    @classmethod
-    def createSubParser(cls, subparsers):
+    def _setResultTrackFormat(self):
         """
-        Creates a subparser. Used by GTool
-        :param subparsers:
-        :return: None
-        """
-        parser = subparsers.add_parser('Flank', help='Creates flanking '
-                                                     'segments of a given '
-                                                     'size')
-        parser.add_argument('track', help='File path of track')
-        parser.add_argument('genome', help='File path of Genome definition')
-        parser.add_argument('-b', type=int, dest='both')
-        parser.add_argument('-s', type=int, dest='start')
-        parser.add_argument('-e', type=int, dest='end')
-        parser.add_argument('-f', '--fractions', action='store_true',
-                            dest='fractions',
-                            help="Instead if flanks a given nr of base "
-                                 "pairs, create them as a fraction of the "
-                                 "features length." )
-        parser.add_argument('-u', '--useStrands', action='store_true',
-                            dest='useStrands',
-                            help="Flow the strand direction when creating "
-                                 "the flanking segments")
-        parser.add_argument('--missingAsNegative', action='store_true',
-                            dest='treatMissingAsNegative',
-                            help="Treat any features with missing strand "
-                                 "information as if they ware negative. The "
-                                 "default is to treat them as positive")
-        parser.add_argument('--resultAllowOverlap', action='store_true',
-                            dest='resultAllowOverlaps',
-                            help="Allow overlap in the resulting track")
-        parser.set_defaults(which='Flank')
-
-    def printResult(self):
-        """
-        Operation returns track, not in use
+        Create the correct TrackFormat for the output track.
+        TODO, do this dynamic
         :return:
         """
+        self._resultTrackFormat = TrackFormat(startList=[], endList=[])
         pass
+
+    def _getKwArgumentInfoDict(self):
+        print("In flank getKwArgument")
+        return OrderedDict([
+            ('both', KwArgumentInfo('both', 'b',
+                                    'Creating flank in both directions.',
+                                    float, None)),
+            ('upstream', KwArgumentInfo('upstream', 'u', 'Size of the '
+                                        'upstream flank. In number '
+                                        'of base pairs', float, None)),
+            ('downstream', KwArgumentInfo('downstream', 'd',
+                                          'Size of the downstream flank. In '
+                                          'number of base pairs',
+                                          float, None)),
+            ('resultAllowOverlap', KwArgumentInfo('resultAllowOverlap','o',
+                                                  'Allow overlap in the '
+                                                  'result track.', bool,
+                                                  False)),
+            ('useFraction', KwArgumentInfo('useFraction', 'f',
+                                           'Interpret flak size as a '
+                                           'fraction of the element size',
+                                           bool, False)),
+            ('useStrands', KwArgumentInfo('useStrands', 's',
+                                          'Follow the strand direction',
+                                          bool, True)),
+            ('treatMissingAsNegative',
+             KwArgumentInfo('treatMissingAsNegative', 'n',
+                            'Treat any missing strand as if they are '
+                            'negative. The default is to treat them as positive',
+                            bool, False)),
+            ('debug', KwArgumentInfo('debug', 'd', 'Print debug info', bool,
+                                     False))
+            ])
