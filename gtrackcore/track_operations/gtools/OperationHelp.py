deb2578188658cfb56e45589cb845e33c1358a10
Radmila Kompova <radmilko@ifi.uio.no>
2019-02-21 10:49:28 +0100

    better help printing

--- gtrackcore/track_operations/gtools/OperationHelp.py
+++ gtrackcore/track_operations/gtools/OperationHelp.py
@@ -18,18 +18,23 @@ class OperationHelp(object):
         for i, v in enumerate(self._info['trackHelp']):
             params.append('track{}'.format(i))
             trackStr += 'track{}: '.format(i) + v + '\n'
-        usageStr += '(' + ','.join(params) +  ')'
+        for k, kw in self._kwArgs.iteritems():
+            #required param
+            if kw.shortkey is None:
+                params.append(kw.key)
+        usageStr += '(' + ', '.join(params) + ')'
         return usageStr + '\n' + trackStr
 
     def getKwArgHelp(self):
         kwArgStr = 'Keyword arguments: \n'
         for k, kw in self._kwArgs.iteritems():
-            helpStr = kw.key + ' ({})'
+            helpStr = kw.key + ' ({}): '
             if kw.shortkey is None:
                 # required
                 helpStr = helpStr.format('required')
             else:
                 helpStr = helpStr.format('optional')
+            helpStr += kw.help
             if kw.contentType is bool:
                 if kw.defaultValue:
                     # default is false
@@ -37,6 +42,6 @@ class OperationHelp(object):
                 else:
                     helpStr += ', default value: True'
 
-            kwArgStr += helpStr + '\n'
+            kwArgStr += '\t' + helpStr + '\n'
 
         return kwArgStr
